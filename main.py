3ac43447287a69143dfc8d86f63a711c504751bd
ViviCarta <cesinev.ma@gmail.com>
2024-05-02 20:05:48 -0400

    VALIDATION AND REFACTORING
    Added Validation Error Prompts
    Consistency with Naming Conventions & Comments

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-# Standard library imports
+"""Standard library imports"""
 import requests
 
-# Third-party library imports
+"""Third-party library imports"""
 from tkinter import *
 from PIL import ImageTk, Image
 import customtkinter as ctk
 # import pywinstyles
 
-# Local imports
+"""Local imports"""
 from weather_manager import WeatherManager
 from utility import _from_rgb
 from config import api_key
 
-# Constants
-BACKGROUND_IMAGE_PATH = "images/background_image.jpg"
-REFRESH_IMAGE_PATH = "images/refresh.png"
-SEARCH_IMAGE_PATH = "images/search.png"
-MAIN_FRAME_WIDTH = 850
-MAIN_FRAME_HEIGHT = 450
+"""Constants"""
+BACKGROUND_IMAGE_PATH: str = "images/background_image.jpg"
+REFRESH_IMAGE_PATH: str = "images/refresh.png"
+SEARCH_IMAGE_PATH: str = "images/search.png"
+MAIN_FRAME_WIDTH: int = 850
+MAIN_FRAME_HEIGHT: int = 450
 
 class WeatherApp(ctk.CTk):
     """Displays the main application window"""
@@ -28,28 +28,28 @@ class WeatherApp(ctk.CTk):
         self.resizable(False, False)
         self.title("Tempest Tracker")
         
-        # Load background image
+        """Load background image"""
         self.background_img = ImageTk.PhotoImage(Image.open(BACKGROUND_IMAGE_PATH))
         self.background_label = Label(self, image=self.background_img, bg="white")
         self.background_label.pack(expand=True, fill="both")
         
-        # Set appearance mode
+        """Set appearance mode"""
         ctk.set_appearance_mode("light")
 
-        # Instantiate WeatherManager
+        """Instantiate WeatherManager"""
         self.weather_manager = WeatherManager(api_key)
 
-        # Create main frame with transparent corners
+        """Create main frame with transparent corners"""
         self.create_main_frame()
         self.create_widgets()
 
-    def create_main_frame(self):
+    def create_main_frame(self) -> None:
         self.main_frame = ctk.CTkFrame(self, width=MAIN_FRAME_WIDTH, height=MAIN_FRAME_HEIGHT, fg_color="#257281", bg_color="#FFFFFF", corner_radius=30)
         self.main_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
         #pywinstyles.set_opacity(self.main_frame, color="#000001", value=0.7)
 
-    def create_widgets(self):
-        "Create an entry widget for user input"
+    def create_widgets(self) -> None:
+        """Create an entry widget for user input"""
         self.weather_entry = ctk.CTkEntry(self.main_frame, width=550, height=50, fg_color="white",
                                            bg_color="#257281", placeholder_text="Type the location here...", 
                                            placeholder_text_color="black", corner_radius=30,
@@ -57,23 +57,23 @@ class WeatherApp(ctk.CTk):
                                            border_color="black")
         self.weather_entry.place(x=150, y=100)
         
-        "Create a search and refresh button"
+        """Create a search and refresh button"""
         self.search_img = ImageTk.PhotoImage(Image.open(SEARCH_IMAGE_PATH).resize((18, 18)))
-        self.search_button = self.create_button(self.weather_entry, "Search", self.search_img, self.updateWeather, 280, 10)
+        self.search_button = self.create_button(self.weather_entry, "Search", self.search_img, self.update_weather, 280, 10)
         self.refresh_img = ImageTk.PhotoImage(Image.open(REFRESH_IMAGE_PATH).resize((18, 18)))
         self.refresh_button = self.create_button(self.weather_entry, "Refresh", self.refresh_img, self.update_info_labels, 400, 10)
         
-        "Data frame within main frame that contains widgets"
+        """Data frame within main frame that contains widgets"""
         self.data_frame = ctk.CTkFrame(self.main_frame, width=720, height=200,
                                        fg_color="white", bg_color="#257281", 
                                        border_color="black", border_width=2, corner_radius=30)
         self.data_frame.place(x=70, y=200)
         
-        "Labels for weather data"
+        """Labels for weather data"""
         self.temp_heading = self.create_heading_label(self.data_frame, "Temperature", 180, 15)
         self.temp_result = self.create_info_label(self.data_frame, f"{self.weather_manager.temperature}{self.weather_manager.temperature_unit}", 180, 40)
         
-        "Switch for changing the temperature display unit"
+        """Switch for changing the temperature display unit"""
         self.switch_var = ctk.StringVar(value="on")
         self.temp_switch = ctk.CTkSwitch(self.data_frame, text="", width=25,
                                          progress_color="black",
@@ -82,11 +82,11 @@ class WeatherApp(ctk.CTk):
                                          command=self.toggle_temperature)
         self.temp_switch.place(x=300, y=18)
         
-        "Headings and Labels for Wind Speed"
+        """Headings and Labels for Wind Speed"""
         self.wind_heading = self.create_heading_label(self.data_frame, "Wind Speed", 400, 15)
         self.wind_result = self.create_info_label(self.data_frame, f"{self.weather_manager.windspeed}{self.weather_manager.wind_unit}", 400, 40)
         
-        "Switch for changing the wind speed display unit"
+        """Switch for changing the wind speed display unit"""
         self.switch_var = ctk.StringVar(value="on")
         self.wind_switch = ctk.CTkSwitch(self.data_frame, text="", width=25,
                                          progress_color="black",
@@ -95,19 +95,19 @@ class WeatherApp(ctk.CTk):
                                          command=self.toggle_windspeed)
         self.wind_switch.place(x=520, y=18)
 
-        "Headings and Labels for Current Location"
+        """Headings and Labels for Current Location"""
         self.location_heading = self.create_heading_label(self.data_frame, "Location", 90, 90)
         self.location_result = self.create_info_label(self.data_frame, f"{self.weather_manager.location}", 90, 115)
 
-        "Headings and Labels for Humidity"
+        """Headings and Labels for Humidity"""
         self.humidity_heading = self.create_heading_label(self.data_frame, "Humidity", 310, 90)
         self.humidity_result = self.create_info_label(self.data_frame, f"{self.weather_manager.humidity}%", 310, 115)
 
-        "Headings and Labels for Weather Condition"
+        """Headings and Labels for Weather Condition"""
         self.condition_heading = self.create_heading_label(self.data_frame, "Condition", 510, 90)
         self.condition_result = self.create_info_label(self.data_frame, f"{self.weather_manager.condition}", 510, 115)
 
-    def create_heading_label(self, parent, text, x, y):
+    def create_heading_label(self, parent, text, x, y) -> ctk.CTkLabel:
         """
         Single method used for header creation 
         so all headers have the same styling
@@ -127,7 +127,7 @@ class WeatherApp(ctk.CTk):
         label.place(x=x, y=y)
         return label
     
-    def create_info_label(self, parent, text, x, y):
+    def create_info_label(self, parent, text, x, y) -> ctk.CTkLabel:
         """
         Single method used for creating info labels
         so all info labels have the same styling
@@ -148,7 +148,7 @@ class WeatherApp(ctk.CTk):
         return label
 
 
-    def create_button(self, parent, text, image, command, x, y):
+    def create_button(self, parent, text, image, command, x, y) -> ctk.CTkButton:
         """
         Single method used for button creation
         so all buttons have the same styling
@@ -172,28 +172,28 @@ class WeatherApp(ctk.CTk):
         #pywinstyles.set_opacity(button, color="#000001", value=1)
         return button
 
-    def updateWeather(self):
+    def update_weather(self) -> None:
         """
         This method gets user input from the entry box
         and makes an API call thorugh weather_manager.getData()
         Then the info labels display the resulting data.
         """        
-        user_input = self.weather_entry.get()  # Retrieve text from the beginning to the end
+        user_input = self.weather_entry.get()
         print(f"USER_INPUT: {user_input}")
         self.weather_manager.getData(user_input)
         print(self.weather_manager)
 
         self.update_info_labels()
 
-    def toggle_temperature(self):
-        self.weather_manager.convertTemperature()
+    def toggle_temperature(self) -> None:
+        self.weather_manager.convert_temperature()
         self.update_info_labels()
 
-    def toggle_windspeed(self):
-        self.weather_manager.convertWindspeed()
+    def toggle_windspeed(self) -> None:
+        self.weather_manager.convert_windspeed()
         self.update_info_labels()
 
-    def update_info_labels(self):
+    def update_info_labels(self) -> None:
         # Update all labels (When the Refresh Button is Pressed)
         self.temp_result.configure(text=f"{self.weather_manager.temperature}°{self.weather_manager.temperature_unit}")
         self.wind_result.configure(text=f"{self.weather_manager.windspeed}{self.weather_manager.wind_unit}")
